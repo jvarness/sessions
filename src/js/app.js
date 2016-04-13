c9474dca83135bd00b5a3df4e32027aff6c9f18d
Jake Varness <jake.varness@gmail.com>
2016-04-12 20:52:53 -0500

    Releasable product

---
+++ src/js/app.js
@@ -0,0 +1,69 @@
+var myAPIKey = '942fea5e2f107364c985ae435c55de53';
+
+function fetchWeather(latitude, longitude) {
+  var req = new XMLHttpRequest();
+  req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?' +
+    'lat=' + latitude + '&lon=' + longitude + '&cnt=1&appid=' + myAPIKey, true);
+  req.onload = function () {
+    if (req.readyState === 4) {
+      if (req.status === 200) {
+        console.log(req.responseText);
+        var response = JSON.parse(req.responseText);
+        var temperature = Math.round((response.main.temp - 273.15) * (9.0 / 5.0) + 32.0);
+        console.log(temperature);
+        Pebble.sendAppMessage({
+          'weatherTemp': temperature + '\xB0F',
+        });
+      } else {
+        console.log('Error');
+      }
+    }
+  };
+  req.send(null);
+}
+
+function locationSuccess(pos) {
+  var coordinates = pos.coords;
+  fetchWeather(coordinates.latitude, coordinates.longitude);
+}
+
+function locationError(err) {
+  console.warn('location error (' + err.code + '): ' + err.message);
+  Pebble.sendAppMessage({
+    'weatherTemp': '--\xB0F'
+  });
+}
+
+var locationOptions = {
+  'timeout': 15000,
+  'maximumAge': 60000
+};
+
+Pebble.addEventListener('ready', function (e) {
+  console.log('connect!' + e.ready);
+  window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+  console.log(e.type);
+});
+
+Pebble.addEventListener('appmessage', function (e) {
+  window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+  console.log(e.type);
+  console.log('message!');
+});
+
+Pebble.addEventListener('showConfiguration', function() {
+  Pebble.openURL('https://jvarness.github.io/sessions/');  
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  var configData = JSON.parse(decodeURIComponent(e.response));
+  
+  console.log('Recieved from app: ' + JSON.stringify(configData));
+  
+  if(configData.accentColor1){
+    Pebble.sendAppMessage({
+      "accentColor1": parseInt(configData.accentColor1, 16),
+      "accentColor2": parseInt(configData.accentColor2, 16),
+    }); 
+  }
+});
--- src/sessions.c
+++ src/sessions.c
@@ -1,11 +1,18 @@
 #include <pebble.h>
 
+#define KEY_ACCENT_1 0
+#define KEY_ACCENT_2 1
+#define KEY_WEATHER_TEMP 2
+
 static Window *window;
 static TextLayer *date_text;
 static TextLayer *clock_text;
 static TextLayer *steps_text;
+static TextLayer *temp_text;
 static GFont lobster_font_30;
 static GFont lobster_font_18;
+static GColor accent_1;
+static GColor accent_2;
 
 static void update_time() {
   time_t now = time(NULL);
@@ -31,6 +38,66 @@ static void update_health() {
   text_layer_set_text(steps_text, buffer);
 }
 
+static void update_weather() {
+  static char buffer[8];
+  
+  persist_read_string(KEY_WEATHER_TEMP, buffer, sizeof(buffer));
+  
+  text_layer_set_text(temp_text, buffer);
+}
+
+static void trigger_app_message() {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+
+  if (!iter) {
+    // Error creating outbound message
+    return;
+  }
+
+  int value = 1;
+  dict_write_int(iter, 1, &value, sizeof(int), true);
+  dict_write_end(iter);
+
+  app_message_outbox_send();
+}
+
+static void sessions_inbox_handler(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "In handler");
+  Tuple *accent_1_t = dict_find(iterator, KEY_ACCENT_1);
+  Tuple *accent_2_t = dict_find(iterator, KEY_ACCENT_2);
+  Tuple *weather = dict_find(iterator, KEY_WEATHER_TEMP);
+  
+  if (accent_1_t) {
+    int color = accent_1_t->value->int32;
+    
+    persist_write_int(KEY_ACCENT_1, color);
+    accent_1 = GColorFromHEX(color);
+    
+    text_layer_set_text_color(clock_text, accent_1);
+    text_layer_set_text_color(temp_text, accent_1);
+    text_layer_set_background_color(date_text, accent_1);
+    text_layer_set_background_color(steps_text, accent_1);
+  }
+  
+  if (accent_2_t) {
+    int color = accent_2_t->value->int32;
+    
+    persist_write_int(KEY_ACCENT_2, color);
+    accent_2 = GColorFromHEX(color);
+    
+    text_layer_set_text_color(date_text, accent_2);
+    text_layer_set_text_color(steps_text, accent_2);
+    window_set_background_color(window, accent_2);
+  }
+  
+  if(weather) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "Weather temp: %s", weather->value->cstring);
+    persist_write_string(KEY_WEATHER_TEMP, weather->value->cstring);
+    update_weather();
+  }
+}
+
 static void load_window(Window *win) {
   Layer *win_layer = window_get_root_layer(window);
   GRect dimensions = layer_get_bounds(win_layer);
@@ -38,24 +105,31 @@ static void load_window(Window *win) {
   clock_text = text_layer_create(GRect(0, dimensions.size.h / 2 - 18, dimensions.size.w, 45));
   text_layer_set_text_alignment(clock_text, GTextAlignmentCenter);
   text_layer_set_background_color(clock_text, GColorClear);
-  text_layer_set_text_color(clock_text, GColorCyan);
+  text_layer_set_text_color(clock_text, accent_1);
   text_layer_set_font(clock_text, lobster_font_30);
   
   date_text = text_layer_create(GRect(0, 0, dimensions.size.w, 25));
-  text_layer_set_background_color(date_text, GColorCyan);
-  text_layer_set_text_color(date_text, GColorBlack);
+  text_layer_set_background_color(date_text, accent_1);
+  text_layer_set_text_color(date_text, accent_2);
   text_layer_set_font(date_text, lobster_font_18);
   text_layer_set_text_alignment(date_text, GTextAlignmentCenter);
   
   steps_text = text_layer_create(GRect(0, dimensions.size.h - 25, dimensions.size.w, 25));
-  text_layer_set_background_color(steps_text, GColorCyan);
-  text_layer_set_text_color(steps_text, GColorBlack);
+  text_layer_set_background_color(steps_text, accent_1);
+  text_layer_set_text_color(steps_text, accent_2);
   text_layer_set_font(steps_text, lobster_font_18);
   text_layer_set_text_alignment(steps_text, GTextAlignmentCenter);
+  
+  temp_text = text_layer_create(GRect(0, dimensions.size.h / 2 + 25, dimensions.size.w, 25));
+  text_layer_set_text_alignment(temp_text, GTextAlignmentCenter);
+  text_layer_set_background_color(temp_text, GColorClear);
+  text_layer_set_text_color(temp_text, accent_1);
+  text_layer_set_font(temp_text, lobster_font_18);
   
   layer_add_child(win_layer, text_layer_get_layer(clock_text));
   layer_add_child(win_layer, text_layer_get_layer(steps_text));
   layer_add_child(win_layer, text_layer_get_layer(date_text));
+  layer_add_child(win_layer, text_layer_get_layer(temp_text));
   
   update_time();
   update_health();
@@ -63,6 +137,10 @@ static void load_window(Window *win) {
 
 static void tick_handler(struct tm *tick_time, TimeUnits changed) {
   update_time();
+  
+  if(changed & HOUR_UNIT) {
+    trigger_app_message();
+  }
 }
 
 static void health_trigger(HealthEventType eventType, void * context) {
@@ -74,11 +152,30 @@ static void health_trigger(HealthEventType eventType, void * context) {
 static void unload_window(Window *win) {
   text_layer_destroy(clock_text);
   text_layer_destroy(steps_text);
+  text_layer_destroy(date_text);
+  text_layer_destroy(temp_text);
   fonts_unload_custom_font(lobster_font_30);
   fonts_unload_custom_font(lobster_font_18);
 }
 
+static void check_defaults() {
+  if(!persist_exists(KEY_ACCENT_1)) {
+    persist_write_int(KEY_ACCENT_1, 0x00FFFF);
+  }
+  if(!persist_exists(KEY_ACCENT_2)) {
+    persist_write_int(KEY_ACCENT_2, 0x000000);
+  }
+  if(!persist_exists(KEY_WEATHER_TEMP)) {
+    persist_write_string(KEY_WEATHER_TEMP, "--\u00B0F");
+  }
+}
+
 static void sessions_init() {
+  check_defaults();
+  
+  accent_1 = GColorFromHEX(persist_read_int(KEY_ACCENT_1));
+  accent_2 = GColorFromHEX(persist_read_int(KEY_ACCENT_2));
+  
   window = window_create();
   
   window_set_window_handlers(window, (WindowHandlers) {
@@ -89,18 +186,22 @@ static void sessions_init() {
   lobster_font_30 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_LOBSTER_30));
   lobster_font_18 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_LOBSTER_18));
   
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  app_message_register_inbox_received(sessions_inbox_handler);
+  app_message_open(128, 128);
+  
+  tick_timer_service_subscribe(MINUTE_UNIT | HOUR_UNIT, tick_handler);
   health_service_events_subscribe(health_trigger, NULL);
   
-  window_set_background_color(window, GColorBlack);
+  window_set_background_color(window, accent_2);
   
   window_stack_push(window, false);
 }
 
 static void sessions_deinit() {
-  window_destroy(window);
   tick_timer_service_unsubscribe();
   health_service_events_unsubscribe();
+  app_message_deregister_callbacks();
+  window_destroy(window);
 }
 
 int main(void) {
